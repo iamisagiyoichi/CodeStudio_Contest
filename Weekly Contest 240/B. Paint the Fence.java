448f79d806007ea9774711373d51b75860653131
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-23 20:56:58 +0530

    Update B. Paint the Fence.java

--- Weekly Contest 240/B. Paint the Fence.java
+++ Weekly Contest 240/B. Paint the Fence.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class Solution {
+    public static long paintFence(int n, int k){
+        // Write your code here
+        if(n == 1) return k;
+        if(n == 2) return 1L*k*k;
+        long same = k;
+        long diff = 1L*k*(k-1);
+        for(int i=3;i<=n;i++){
+            long newSame = diff;
+            long newDiff = (same+diff)*(k-1);
+            same = newSame;
+            diff = newDiff;
+        }
 
+        return same + diff;
+    }
+
+}
