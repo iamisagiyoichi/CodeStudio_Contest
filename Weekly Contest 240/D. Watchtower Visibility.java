0665ab330cbc5d61e8e08995c261289a9418574f
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-23 20:57:47 +0530

    Update D. Watchtower Visibility.java

--- Weekly Contest 240/D. Watchtower Visibility.java
+++ Weekly Contest 240/D. Watchtower Visibility.java
@@ -1,1 +1,21 @@
+import java.util.Stack;
+public class Solution {
+    public static int[] watchtowerVisibility(int[] heights){
+        // Write your code here
+        int n = heights.length;
+        int[] ans = new int[n];
+        Stack<Integer>st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            int cnt = 0;
+            while(!st.isEmpty() && heights[i]>st.peek()){
+                st.pop();
+                cnt++;
+            }
+            if(!st.isEmpty())cnt++;
+            ans[i] = cnt;
+            st.push(heights[i]);
+        }
+        return ans;
+    }
 
+}
