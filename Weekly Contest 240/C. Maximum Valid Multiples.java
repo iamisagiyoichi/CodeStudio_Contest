5a4d363fc2991f507c1dd28e7dc43fa5285599bc
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-23 20:57:27 +0530

    Update C. Maximum Valid Multiples.java

--- Weekly Contest 240/C. Maximum Valid Multiples.java
+++ Weekly Contest 240/C. Maximum Valid Multiples.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class Solution {
+    public static long maximumValidMultiple(long value,long step){
+        // Write your code here
+        if(step<=0)return 0;
+        long ans = 0;
+        while(value >= step){
+            value -= step;
+            ans += step;
+        }
 
+        return ans;
+    }
+}
