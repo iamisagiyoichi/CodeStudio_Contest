f5ac5b03e271931f25702445642fbc2ff2107a55
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-30 20:46:32 +0530

    Update A. Is Containing Duplicates.java

--- Weekly Contest 241/A. Is Containing Duplicates.java
+++ Weekly Contest 241/A. Is Containing Duplicates.java
@@ -1,1 +1,22 @@
+/* Time Complexity: O(n)
+   Space Complexity: O(n)
+*/
 
+import java.util.*;
+
+public class Solution {
+
+    public static boolean containsDuplicate(ArrayList<Integer> nums) {
+
+        // Write your code here.
+        HashSet<Integer>set = new HashSet<>();
+        for(int x:nums){
+            if(set.contains(x)){
+                return true;
+            }
+            set.add(x);
+        }
+
+        return false;
+    }
+}
