49fd49b00809c7b4126cb1d1b7336524ab3b8861
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-30 20:46:50 +0530

    Implement mergeSortedArrays to merge two sorted arrays

--- Weekly Contest 241/B. Merge Two Sorted Arrays.java
+++ Weekly Contest 241/B. Merge Two Sorted Arrays.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Solution {
+
+    public static ArrayList<Integer> mergeSortedArrays(ArrayList<Integer> arr1,ArrayList<Integer> arr2) {
+        ArrayList<Integer>ans = new ArrayList<>();
+        int i = 0, j = 0;
+        while(i < arr1.size() && j < arr2.size()){
+            if(arr1.get(i) <= arr2.get(j)){
+                ans.add(arr1.get(i));
+                i++;
+            }
+            else{
+                ans.add(arr2.get(j));
+                j++;
+            }
+        }
+        while(i<arr1.size()){
+            ans.add(arr1.get(i));
+            i++;
+        }
+        while(j<arr2.size()){
+            ans.add(arr2.get(j));
+            j++;
+        }
+        return ans;
+    }
+}
