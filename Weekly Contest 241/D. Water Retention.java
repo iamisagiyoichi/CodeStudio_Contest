8b66881bb8bb28d448de3681b1e4f7d605023f5c
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-30 20:47:23 +0530

    Update D. Water Retention.java

--- Weekly Contest 241/D. Water Retention.java
+++ Weekly Contest 241/D. Water Retention.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+public class Solution {
+
+    public static int trap(ArrayList<Integer> height) {
+        // Write your code here
+        int  l = 0, r = height.size()-1;
+        int lm = 0 , rm = 0;
+        int ans = 0;
+        while(l <= r){
+            if(height.get(l) <= height.get(r)){
+                if(height.get(l) >= lm){
+                    lm= height.get(l);
+                }
+                else{
+                    ans += lm - height.get(l);
+                }
+                l++;
+            }
+            else{
+                if(height.get(r) >= rm){
+                    rm = height.get(r);
+                }
+                else{
+                    ans += rm - height.get(r);
+                }
+                r--;
+            }
+        }
+        return ans;
+    }
+}
