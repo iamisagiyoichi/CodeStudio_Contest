c773f05e966fb7ef39aa3b79311edfa1343de7c7
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-30 20:47:08 +0530

    Update C. Three Value Sorting.java

--- Weekly Contest 241/C. Three Value Sorting.java
+++ Weekly Contest 241/C. Three Value Sorting.java
@@ -1,1 +1,29 @@
+public class Solution {
 
+    public static int[] sortValues(int[] nums){
+
+        // Write your code here
+        int low = 0, mid = 0, high = nums.length-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+
+        return nums;
+    }
+
+}
